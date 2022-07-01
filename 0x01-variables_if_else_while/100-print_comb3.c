2a20cc22191f4c768bc256cdbdd02b0d7fc138b7
Wanjiru27 <mwanjiru736@gmail.com>
2022-07-01 15:15:13 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of two digits
+ *
+ * Description: print double digit combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+
+		i++
+	}
+
+	putchar(10);
+
+	return (0);
+}
