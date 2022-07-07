77a850bd642cdfb14d950c7ec46ce6f420b74557
Wanjiru27 <mwanjiru736@gmail.com>
2022-07-07 08:51:57 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@
  */
 void print_most_numbers(void)
 {
-	int i = '0';
+	int i;
 
-	for (; i <= '9'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (i != '2' && i != '4')
-			_putchar(i);
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
 	}
 	_putchar('/n');
 }
