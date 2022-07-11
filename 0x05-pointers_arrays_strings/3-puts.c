d193fd2d7828fd37893c899a29e79a4fa84b6762
Wanjiru27 <mwanjiru736@gmail.com>
2022-07-11 08:03:22 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _puts - print to the stdout
@@ -11,9 +12,11 @@ void _puts(char *str)
 	int len, i;
 
 	len = strlen(str);
+
 	for (i = 0; i < len; i++)
 	{
 		_putchar(str[i]);
+
 	}
 	_putchar('\n');
 }
