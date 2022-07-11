39517bd4f10e4e44f27feab82c752eb654aa7c4b
Wanjiru27 <mwanjiru736@gmail.com>
2022-07-11 07:00:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take pointer and update value to 98
+ * @n: pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void reset_to_98(int *n);
