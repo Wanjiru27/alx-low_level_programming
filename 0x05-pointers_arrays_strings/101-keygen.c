5d5231e4c1946693aac9b5745d1393109eb0061c
Wanjiru27 <mwanjiru736@gmail.com>
2022-07-13 01:17:14 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	int pass, sum;
+	int r = 0, c = 0;
+	time_t t;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
+	srand((unsigned int) time(&t));
+	while (c < 2772)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
 	}
-	printf("%c", 2772 - sum);
-
+	printf("%c\n", (2772 - c));
 	return (0);
 }
